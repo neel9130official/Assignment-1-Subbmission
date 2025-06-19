f5cc5ef5d6984fc6321893380f01956c6423783f
NEELKANTH MUNDKAR <126777881+neel9130official@users.noreply.github.com>
2025-06-19 23:40:19 +0530

    Add files via upload

---
+++ Assignment1_Task1.py
@@ -0,0 +1,14 @@
+                   
+
+a=input("Enter he first number : ")
+a=int(a)
+b=input("Enter the second number : ")
+b=int(b)
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("\nAddition:",add)
+print("Substraction:",sub)
+print("Multiplication:",mul)
+print("Division:",div)
---
+++ Assignment1_Task2.py
@@ -0,0 +1,7 @@
+
+first_name=input("Enter your first name:")
+fn=str(first_name)
+last_name=input("Enter your first name:")
+ln=str(last_name)
+
+print("Hello,"+fn+" "+ln+"!"+" Welcome to the python program.")
